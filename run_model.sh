f28f1fa2fe328fd6e3a66d50ea2a3eff2f36c93b
natan-matos <natanclimaco@gmail.com>
2024-03-01 10:22:18 -0300

    pyenv path modified

--- run_model.sh
+++ run_model.sh
@@ -3,7 +3,7 @@ data=$(date +'%Y-%m-%dT%H:%M:%S')
 
 # path
 path='/home/ubuntu/deploymentoria'
-path_to_envs='/home/ubuntu/.local/bin/'
+path_to_envs='/home/ubuntu/.pyenv/shims'
 
 
 # run Jupyter Notebook using papermill
