796963becf51f7802f7165123b41177873102da1
natan-matos <natanclimaco@gmail.com>
2024-03-01 10:12:04 -0300

    pyenv path modified

--- run_model.sh
+++ run_model.sh
@@ -2,8 +2,8 @@
 data=$(date +'%Y-%m-%dT%H:%M:%S')
 
 # path
-path = '/home/unbuntu/deploymentoria'
-path_to_envs = '/home/ubuntu/.pyenv/shims'
+path = '/home/ubuntu/deploymentoria'
+path_to_envs = '/home/ubuntu/.pyenv/shims/'
 
 # run Jupyter Notebook using papermill
 $path_to_envs papermil $path/notebooks/deploy-mentoria.ipynb $path/reports/deploy-mentoria-$data.ipynb
