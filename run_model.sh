576086c7cf73befb9e794569173c0ba6878d2eee
natan-matos <natanclimaco@gmail.com>
2024-03-01 09:41:35 -0300

    pyevn source removed

--- run_model.sh
+++ run_model.sh
@@ -1,13 +1,14 @@
 # variable
 data=$(date +'%Y-%m-%dT%H:%M:%S')
 
-# conda path
-pyenv_path='/home/ubuntu/.pyenv'
+# pyenv path
+#pyenv_path='/home/ubuntu/.pyenv'
 
 # activate env
-source $pyenv_path/libexec/pyenv
+#source $pyenv_path/libexec/pyenv
 
 # cluster env
 pyenv activate clusterenv
 
-#papermill /home/ubuntu/deploymentoria/notebooks/deploy-mentoria.ipynb /home/ubuntu/deploymentoria/notebooks/deploy-mentoria-$data.ipynb
+papermill_path=$(wich papermill)
+#$papermill_path /home/ubuntu/deploymentoria/notebooks/deploy-mentoria.ipynb /home/ubuntu/deploymentoria/notebooks/deploy-mentoria-$data.ipynb
