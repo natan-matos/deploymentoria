9871fa9ee8a995671d79890c9741a508bbabd4da
natan-matos <natanclimaco@gmail.com>
2024-03-01 09:54:40 -0300

    pyenv path modified

--- run_model.sh
+++ run_model.sh
@@ -1,19 +1,21 @@
-# load pyenv 
-export PATH="$HOME/.pyenv/bin:$PATH"
-eval "$(pyenv init --path)"
-eval "$(pyenv virtualenv-init -)"
-
 # variable
 data=$(date +'%Y-%m-%dT%H:%M:%S')
 
 # pyenv path
-#pyenv_path='/home/ubuntu/.pyenv'
+pyenv_path='/home/ubuntu/.pyenv'
+
+# Load pyenv into the shell
+export PATH="$pyenv_path/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
 
 # activate env
-#source $pyenv_path/libexec/pyenv
-
-# cluster env
 pyenv activate clusterenv
 
+# full path for papermill
 papermill_path=$(which papermill)
-#$papermill_path /home/ubuntu/deploymentoria/notebooks/deploy-mentoria.ipynb /home/ubuntu/deploymentoria/notebooks/deploy-mentoria-$data.ipynb
+
+# run Jupyter Notebook using papermill
+$papermill_path /home/ubuntu/deploymentoria/notebooks/deploy-mentoria.ipynb /home/ubuntu/deploymentoria/notebooks/deploy-mentoria-$data.ipynb
+
+
