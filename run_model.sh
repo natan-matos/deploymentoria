a67935971f454949b2739e04d9110b0472fdb479
natan-matos <natanclimaco@gmail.com>
2024-03-01 11:37:37 -0300

    pyenv path modified

--- run_model.sh
+++ run_model.sh
@@ -5,8 +5,6 @@ data=$(date +'%Y-%m-%dT%H:%M:%S')
 path='/home/ubuntu/deploymentoria'
 path_to_envs='/home/ubuntu/.pyenv/shims'
 
-# activate env
-#pyenv activate clusterenv
 
 # run Jupyter Notebook using papermill
 papermill $path/notebooks/deploy-cluster-nm.ipynb $path/reports/deploy-cluster-nm-$data.ipynb
